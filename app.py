a7737c36f7bafb3a20f7cc1fb5676707041ffe10
Rishi <ambwanirishi@gmail.com>
2019-03-05 19:21:41 +0530

    added plotli

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, render_template,redirect ,url_for,session
+from plotly.offline import plot
+from plotly.graph_objs import Scatter
+from flask import Markup
+import time
 import test2
 try:
 	import urllib.request as urllib2
@@ -10,6 +14,9 @@ import re
 app = Flask(__name__, template_folder='website')
 app.secret_key = "5H473893939$385H"
 
+y=[]
+x=[]
+
 
 @app.route('/')
 def my_form():
@@ -20,19 +27,24 @@ def my_form_post():
 	text = request.form['url']
 	session['processed_text'] = text
 	session['val']=0
-	
+
 	return redirect(url_for('data'))
 
 @app.route('/data')
 def data():
+
 	if(session.get('val', None)==0):
-		data=test2.data(session.get('processed_text', None))
+		data,c=test2.data(session.get('processed_text', None))
 	else:
-		data=test2.loop()
+		data,c=test2.loop()
+	y.append(c)
+	x.append(data)
 	session['val']=1
+	my_plot_div = plot([Scatter(x=y,y=x)], output_type='div')
+	print(data)
 
 
-	return render_template('url2.html',data=data)
+	return render_template('url2.html',data=Markup(my_plot_div))
 
 
 
